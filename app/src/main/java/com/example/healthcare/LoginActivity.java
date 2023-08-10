dd710a20e4555dcedf1db330f0c710c9b7bc8c70
sakib <jcse28.387@gmail.com>
2023-08-10 23:41:10 +0600

    Login implementation

---
+++ app/src/main/java/com/example/healthcare/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+   EditText edUseranamem,edPassword;
+   Button btn;
+   TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUseranamem=findViewById(R.id.editTextUserName);
+        edPassword=findViewById(R.id.editTextpASSWORD);
+        btn=findViewById(R.id.buttonLogin);
+        tv=findViewById(R.id.textRgForNewUser);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=edUseranamem.getText().toString();
+                String password=edPassword.getText().toString();
+                if(username.length()==0||password.length()==0){
+                    Toast.makeText(getApplicationContext(),"All  fields are required to fill",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Login button ",Toast.LENGTH_SHORT).show();
+
+                }
+             }
+        });
+    }
+}
